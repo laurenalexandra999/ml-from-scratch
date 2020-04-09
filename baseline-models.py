df09e72228e8ad7b5fd73ff645a46b079755404f
Lauren Pease <lauren@laurenpease.com>
2020-04-08 18:57:20 -0700

    zero rule baseline model

--- baseline-models.py
+++ baseline-models.py
@@ -39,3 +39,55 @@ test = [[None], [None], [None], [None]]
 predictions = random_prediction_algorithm(train, test)
 print(predictions) 
 # [0, 0, 1, 0]
+
+"""
+zero rule algorithm
+
+This algorithm uses information about a problem to create one rule
+to use when making predictions. 
+"""
+
+"""
+Classification 
+
+- one rule: predict the most common class. 
+  e.g. a dataset has 90 instances of class A and 10 of class B.
+  Since class A is the most common, using a zero_rule_classification
+  algorithm would secure a baseline accuracy of 90%. 
+"""
+
+def zero_rule_classification(train, test):
+    output_values = [row[-1] for row in train]
+    # get the class value with the highest count in the training data
+    prediction = max(set(output_values), key=output_values.count)
+    predicted = [prediction for i in range(len(test))]
+    return predicted
+
+# example
+seed(1)
+train = [['0'], ['0'], ['0'], ['0'], ['1'], ['1']]
+test = [[None], [None], [None], [None]]
+predictions = zero_rule_classification(train, test)
+print(predictions)
+# ['0', '0', '0', '0']
+
+"""
+Regression
+
+- one rule: predict the central tendency (mean or median)
+"""
+
+def zero_rule_regression(train, test):
+    output_values = [row[-1] for row in train]
+    # get mean of output values
+    prediction = sum(output_values) / float(len(output_values))
+    predicted = [prediction for i in range(len(test))]
+    return predicted 
+
+# example
+seed(1)
+train = [[10], [15], [12], [15], [18], [20]]
+test = [[None], [None], [None], [None]]
+predictions = zero_rule_regression(train, test)
+print(predictions)
+# [15.0, 15.0, 15.0, 15.0]
