f7c4398dadb2b93a97ac7848ef8186b69225dbcf
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-04-11 18:56:33 -0700

    linear regression example

--- linear-regression.py
+++ linear-regression.py
@@ -117,3 +117,96 @@ rmse = evaluate_algorithm(dataset, simple_linear_regression)
 print('RMSE: %.3f' % (rmse))
 # [1.1999999999999995, 1.9999999999999996, 3.5999999999999996, 2.8, 4.3999999999999995]
 # RMSE: 0.693
+
+"""
+example with csv 
+"""
+
+from random import seed 
+from random import randrange 
+from csv import reader 
+from math import sqrt 
+
+def load_csv(filename):
+    dataset = list() 
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset: 
+        row[column] = float(row[column].strip())
+
+# train test split
+def train_test_split(datset, split): 
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
+
+# calculate root mean squared error
+def rmse_metric(actual, predicted):
+    sum_error = 0.0
+    for i in range(len(actual)):
+        prediction_error = predicted[i] - actual[i]
+        sum_error += (prediction_error ** 2)
+    mean_error = sum_error / float(len(actual))
+    return sqrt(mean_error) 
+
+# evaluate algorithm using train test split 
+def evaluate_algorithm(dataset, algorithm, split, *args):
+    train, test = train_test_split(dataset, split)
+    test_set = list()
+    for row in test:
+        row_copy = list(row)
+        row_copy[-1] = None 
+        test_set.append(row_copy)
+    predicted = algorithm(train, test_set, *args)
+    actual = [row[-1] for row in test]
+    rmse = rmse_metric(actual, predicted)
+    return rmse 
+
+def mean(values): 
+    return sum(values) / float(len(values))
+
+def variance(values, mean):
+    return sum([(x-mean)**2 for x in values])
+
+def covariance(x, mean_x, y, mean_y):
+    covar = 0.0
+    for i in range(len(x)):
+        covar += (x[i] - mean_x) * (y[i] - mean_y)
+    return covar     
+
+def coefficients(dataset):
+    x = [row[0] for row in dataset]
+    y = [row[1] for row in dataset]
+    x_mean, y_mean = mean(x), mean(y)
+    b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
+    b0 = y_mean - b1 * x_mean 
+    return [b0, b1]
+
+def simple_linear_regression(train, test):
+    predictions = list()
+    b0, b1 = coefficients(train)
+    for row in test:
+        yhat = b0 + b1 * row[0]
+        predictions.append(yhat)
+    return predictions 
+
+seed(1)
+filename = 'data.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset, i)
+split = 0.6
+rmse = evaluate_algorithm(dataset, simple_linear_regression, split)
+print('RMSE: %.3f' % (rmse))
+# RMSE: 33.630
