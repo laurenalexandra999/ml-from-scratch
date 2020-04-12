21fd048b4c59101a1d94d9e08a79555a4e7e148b
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-04-11 17:36:51 -0700

    linear regression step 2 covariance

--- linear-regression.py
+++ linear-regression.py
@@ -29,3 +29,25 @@ print('x stats: mean=%.3f variance=%.3f' % (mean_x, var_x))
 print('y stats: mean=%.3f variance=%.3f' % (mean_y, var_y)) 
 # x stats: mean=3.000 variance=10.000
 # y stats: mean=2.800 variance=8.800 
+
+"""
+step 2: calculate covariance. 
+
+- covariance describes how two or more groups of numbers change together
+"""
+
+# find covariance between X and Y
+def covariance(x, mean_x, y, mean_y):
+    covar = 0.0
+    for i in range(len(x)):
+        covar += (x[i] - mean_x) * (y[i] - mean_y)
+    return covar 
+
+# example 
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+x = [row[0] for row in dataset]
+y = [row[1] for row in dataset]
+mean_x, mean_y = mean(x), mean(y) 
+covar = covariance(x, mean_x, y, mean_y)
+print('Covariance: %.3f' % (covar))
+# Covariance: 8.000
