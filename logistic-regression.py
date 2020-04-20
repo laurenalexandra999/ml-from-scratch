c0c5a0dfd9d631bfaa4f4cec8db1677b4027e334
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-04-19 18:57:07 -0700

    predict output with coefficients

--- logistic-regression.py
+++ logistic-regression.py
@@ -32,3 +32,43 @@ stochastic gradient descent
 - yhat is the prediction made by the coefficients
 - x is the input 
 """
+from math import exp 
+
+# predict an output for a row given coefficients
+def predict(row, coefficients):
+    # the bias/intercept is not responsible for an input value
+    yhat = coefficients[0]
+    for i in range(len(row) - 1):
+        # y = b0 + b1 * x 
+        yhat += coefficients[i + 1] * row[i]
+    # yhat = 1.0 / (1.0 + e^-(b0 + b1 * X1))
+    return 1.0 / (1.0 + exp(-yhat)) 
+
+dataset = [[2.7810836,2.550537003,0],
+[1.465489372,2.362125076,0],
+[3.396561688,4.400293529,0],
+[1.38807019,1.850220317,0],
+[3.06407232,3.005305973,0],
+[7.627531214,2.759262235,1],
+[5.332441248,2.088626775,1],
+[6.922596716,1.77106367,1],
+[8.675418651,-0.242068655,1],
+[7.673756466,3.508563011,1]]
+coef = [-0.406605464, 0.852573316, -1.104746259]
+
+for row in dataset: 
+    yhat = predict(row, coef)
+    print("Expected=%.3f, Predicted=%.3f [%d]" % (row[-1], yhat, round(yhat))) 
+
+"""
+Expected=0.000, Predicted=0.299 [0]
+Expected=0.000, Predicted=0.146 [0]
+Expected=0.000, Predicted=0.085 [0]
+Expected=0.000, Predicted=0.220 [0]
+Expected=0.000, Predicted=0.247 [0]
+Expected=1.000, Predicted=0.955 [1]
+Expected=1.000, Predicted=0.862 [1]
+Expected=1.000, Predicted=0.972 [1]
+Expected=1.000, Predicted=0.999 [1]
+Expected=1.000, Predicted=0.905 [1]
+"""
