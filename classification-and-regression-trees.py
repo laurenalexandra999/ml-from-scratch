ecfeb70f4a697666bcc2407d05ea5a4ff817bdd0
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-04-23 20:10:59 -0700

    gini index

--- classification-and-regression-trees.py
+++ classification-and-regression-trees.py
@@ -19,3 +19,60 @@ CART - Classification and Regression Trees
   pure the nodes are, i.e. how mixed up the training data assigned
   to each nodes are 
 """
+
+"""
+Gini Index
+
+- a cost function to divide data into two groups of rows (a split)
+- perfect score is 0. 
+- worst case is a split with 50/50 classes in each group i.e. poor classification. score of 0.5.
+
+gini_index = (1 - sum(proportion)) * (group_size / total_samples)
+
+proportion = count(class_value) / count(rows)
+
+e.g. a perfect split has 2 groups of data with 2 rows in each group.
+the rows in the first group are class 0
+the rows in the second group are class 1
+
+group_1_class_0 = 2/2 = 1
+group_1_class_1 = 0/2 = 0
+group_2_class_0 = 0/2 = 0
+group_2_class_1 = 2/2 = 1
+
+Gini(group_1) = (1 - (1 * 1 + 0 * 0)) * (2/4)
+Gini(group_1) = 0.0 * 0.5
+Gini(group_1) = 0.0 
+Gini(group_2) = (1 - (0 * 0 + 1 * 1)) * (2/4)
+Gini(group_2) = 0.0 * 0.5
+Gini(group_2) = 0.0
+
+Perfect score: 
+group_1 + group_2 = 0.0. 
+"""
+
+# calculate gini index
+def gini_index(groups, classes):
+    # count samples at split
+    n_instances = float(sum([len(group) for group in groups]))
+    # sum weighted index for each group
+    gini = 0.0 
+    for group in groups: 
+        size = float(len(group))
+        # do not divide by zero
+        if size == 0:
+            continue
+        score = 0.0
+        # score the group with each class score 
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        # weight the group score by its size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini 
+
+# example
+print(gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
+print(gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
+# 0.5
+# 0.0
